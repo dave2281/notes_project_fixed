ad19e15a3018c57861bf76b4b077dd854d01384a
dave2281 <dava.bigboy@gmail.com>
2024-09-16 18:00:14 +0300

    changed migration

--- db/migrate/20240916145229_add_color_to_notes.rb
+++ db/migrate/20240916145229_add_color_to_notes.rb
@@ -1,4 +1,4 @@
-class AddColorToNote < ActiveRecord::Migration[7.1]
+class AddColorToNotes < ActiveRecord::Migration[7.1]
   def change
     add_column :notes, :color, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_081302) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_16_145229) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.string "text"
     t.string "tags"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
